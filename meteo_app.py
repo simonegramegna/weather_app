0d0f4cc5b2edbd6070e1edce5327979b12e3953c
simonegramegna <gramegna.simone@gmail.com>
2019-03-22 21:22:33 +0100

    now it is possible to get weather in different countries

--- meteo_app.py
+++ meteo_app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 #reads a file with OpenWeatherapi token
 with open('weather_id.txt','r') as file_id:
     weather_id = str(file_id.read())
+
+#country_dict
+country_dict = {'Italy':'IT','United Kingdom':'UK','Germany':'DE','U.S.A':'US'}
  
 @app.route('/')
 def index():
@@ -19,18 +22,22 @@ def index():
 @app.route('/get_city',methods=['POST'])
 def get_city():
     city_name = request.form['city_input']
+    country_name = request.form['country_input']
+    country_code = country_dict[country_name]
     
-    #Weather requests for now works only with italian cities
-    url = '''http://api.openweathermap.org/data/2.5/weather?q={},IT&APPID=%s'''.format(city_name)%weather_id
+    
+    
+    url = '''http://api.openweathermap.org/data/2.5/weather?q='''+ city_name + ','+ country_code + '''&APPID='''+ weather_id
 
     res = requests.get(url)
     data = res.json()
+    
     if data['cod'] == '404':
-        message = str(city_name) + "\t not found"
+        message = "City\t" + str(city_name) + "\t not found"
         return render_template("home.html",message_error = message)
     else:
         weather_data_description = data['weather'][0]['description']
-        weather_data_response = "Weather in \t "+ str(city_name) +"\tis\t" + str(weather_data_description)
+        weather_data_response = "Weather today in \t "+ str(city_name) +"\tis\t" + str(weather_data_description)
         return render_template("home.html",weather_data = weather_data_response)
 
     return render_template("home.html")
