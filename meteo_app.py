06e1d1427169d31ef3d60aefd96b0bb138d642d5
simonegramegna <gramegna.simone@gmail.com>
2019-03-23 18:40:47 +0100

    Working on new features..still some bugs to fix

--- meteo_app.py
+++ meteo_app.py
@@ -13,7 +13,12 @@ with open('weather_id.txt','r') as file_id:
     weather_id = str(file_id.read())
 
 #country_dict
-country_dict = {'Italy':'IT','United Kingdom':'UK','Germany':'DE','U.S.A':'US'}
+country_dict = {
+'Italy':'IT',
+'United Kingdom':'UK',
+'Germany':'DE',
+'U.S.A':'US'
+}
  
 @app.route('/')
 def index():
@@ -25,20 +30,36 @@ def get_city():
     country_name = request.form['country_input']
     country_code = country_dict[country_name]
     
-    
-    
-    url = '''http://api.openweathermap.org/data/2.5/weather?q='''+ city_name + ','+ country_code + '''&APPID='''+ weather_id
+    url = '''http://api.openweathermap.org/data/2.5/weather?q='''+ city_name + ','+ country_code + '''&APPID='''+ weather_id + '''&units=metric'''
 
     res = requests.get(url)
     data = res.json()
+
+    data_code_response = data['cod']
     
-    if data['cod'] == '404':
+    #sistema il template meglio!!
+    
+    if  data_code_response == '404':
         message = "City\t" + str(city_name) + "\t not found"
         return render_template("home.html",message_error = message)
-    else:
-        weather_data_description = data['weather'][0]['description']
-        weather_data_response = "Weather today in \t "+ str(city_name) +"\tis\t" + str(weather_data_description)
-        return render_template("home.html",weather_data = weather_data_response)
+    if data_code_response == '200':
+
+        #gets weather data
+        weather_description = data['weather'][0]['description']
+        weather_temp = data['main']['temp']  
+        weather_humidity = data['main']['humidity']
+        weather_pressure = data['main']['pressure']
+        weather_windspeed = data['wind']['speed']
+
+        return render_template("home.html",
+        city_name = city_name,
+        weather_description = weather_description,
+        weather_temp = weather_temp, 
+        weather_humidity = weather_humidity,
+        weather_pressure = weather_pressure,
+        weather_windspeed = weather_windspeed
+        )
+    
 
     return render_template("home.html")
   
