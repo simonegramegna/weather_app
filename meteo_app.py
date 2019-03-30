db6ab001d1ebf7e1fc0da6816fb0fe9c10253102
simonegramegna <gramegna.simone@gmail.com>
2019-03-30 18:34:58 +0100

    code improvements

--- meteo_app.py
+++ meteo_app.py
@@ -1,10 +1,11 @@
-from flask  import Flask, render_template, request, flash
+from flask  import Flask, render_template, request, flash, jsonify, url_for
 #from flask_bootstrap import Bootstrap
 #from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, Form, validators
 from wtforms.validators import DataRequired, Length
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
@@ -22,9 +23,6 @@ country_dict = {
 
 
 
-
-
- 
 @app.route('/')
 def index():
     return render_template("weather_template.html")
@@ -62,6 +60,25 @@ def get_city():
         weather_humidity = weather_humidity,
         weather_pressure = weather_pressure,
         weather_windspeed = weather_windspeed )
+
+@app.route('/get_ip',methods=['GET'])
+def get_client_ip():
+    return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+
+#updates css when changed
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
     
 if __name__ == '__main__':
     app.run(debug=True)
